c6ddb94a4bdcf1ebde3a4ee5c9c060068502702f
adithya-tp <adithyatp@yahoo.com>
2020-12-09 21:27:03 +0400

    00272 Tex Quotes

---
+++ 00272/sol.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define forn(i, l, r) for(int i = l; i <= r; i++)
+#define fornr(i, r, l) for(int i = r; i >= l; i--)
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    int x = 1;
+    while(getline(cin, s)) {
+        stringstream ss;
+        for(char c : s) {
+            if(c == '"') {
+                if(x) {
+                    ss << "``";
+                } else {
+                    ss << "''";
+                }
+                x ^= 1;
+            } else {
+                ss << c;
+            }
+        }
+        cout << ss.str() << "\n";
+    }
+    return 0;
+}
