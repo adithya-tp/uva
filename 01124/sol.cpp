a9e70b50b5bc327d12913c21e40ccb616b50ca0f
adithya-tp <adithyatp@yahoo.com>
2020-12-09 21:39:02 +0400

    01124 Celebrity Jeopardy

---
+++ 01124/sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define forn(i, l, r) for(int i = l; i <= r; i++)
+#define fornr(i, r, l) for(int i = r; i >= l; i--)
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    while(getline(cin, s)) {
+        cout << s << "\n";
+    }
+    return 0;
+}
